38e3b4349b049e768b37a10be4325dd94ded2b5d
LESS14 <100519912+LESS14@users.noreply.github.com>
2022-09-05 03:57:35 -0300

    Add files via upload

---
+++ UI/QRCodeWeb/Download.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace SecureQRCodeWeb
+{
+    public partial class Download : System.Web.UI.Page
+    {
+        FileInfo file = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            file = new FileInfo(Request.QueryString["myFile"]);               
+        }    
+
+        protected void btnDownload_Click(object sender, EventArgs e)
+        {
+            Response.ClearContent();
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
+            Response.AddHeader("Content-Length", file.Length.ToString());
+            Response.TransmitFile(file.FullName);
+            Response.End();
+        }
+    }
+}
---
+++ UI/QRCodeWeb/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecureQRCodeWeb.Startup))]
+namespace SecureQRCodeWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app)
+        {
+           
+        }
+    }
+}
